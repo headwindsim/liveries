ce398014f59bded124f98e192f9d25167e888d05
Denis Freund <info@denis-freund.de>
2024-01-16 19:43:52 +0100

    chore: add su95x files

--- igniter.config.mjs
+++ igniter.config.mjs
@@ -11,5 +11,15 @@ export default new TaskOfTasks("all", [
         new TaskOfTasks("dist", [
             new ExecTask("manifests", "npm run build-a339x-livery-package:manifest")
         ])
+    ]),
+    new TaskOfTasks("su95x-livery-package", [
+        // Prepare the out folder and any other pre tasks.
+        // Currently, these can be run in parallel but in the future, we may need to run them in sequence if there are any dependencies.
+        new TaskOfTasks("preparation", [
+            new ExecTask("copy-base-files", "npm run build-su95x-livery-package:copy-base-files")
+        ], true),
+        new TaskOfTasks("dist", [
+            new ExecTask("manifests", "npm run build-su95x-livery-package:manifest")
+        ])
     ])
 ]);
